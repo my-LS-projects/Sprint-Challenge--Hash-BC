b148b27cc8527e054009ea5b5ac9ea21b5d68eb5
Dakotah Huey <hueydakotah@gmail.com>
2020-03-13 13:40:14 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,30 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    # weights = list of weights
+    # length = how many weights
+    # limit = weight limit
+    # return tuple answer with two weights (highest weight, second weight)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        # hashtable, key, value
+        hash_table_insert(ht, weights[i], i)
+
+    for j in range(length):
+        # ht, key
+        diff = hash_table_retrieve(ht, limit - weights[j])
+
+        if value is not None:
+            return (diff, j)
 
     return None
 
